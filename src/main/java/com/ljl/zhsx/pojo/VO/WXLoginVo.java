93a3fb23f89f0e53a63efb5cbfba72989caf8a34
狗卷 <963314043@qq.com>
2022-07-22 10:44:42 +0800

    微信登陆接口

---
+++ src/main/java/com/ljl/zhsx/pojo/VO/WXLoginVo.java
@@ -0,0 +1,13 @@
+package com.ljl.zhsx.pojo.VO;
+
+import lombok.Data;
+
+/**
+ * @author L.J.L
+ * @QQ 963314043
+ * @date 2022/7/21 20:13
+ */
+@Data
+public class WXLoginVo {
+    private String code;
+}
